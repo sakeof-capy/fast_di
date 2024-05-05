b273c7d560285f467005213e36456eb872bc8233
Fourier2718281828 <non-linear@ukr.net>
2024-05-05 20:11:26 +0300

    added: global dynamic di

--- di_global/include/fast_di/global/GlobalDI.hpp
+++ di_global/include/fast_di/global/GlobalDI.hpp
@@ -2,9 +2,50 @@
 #define GLOBALDI_HPP_
 
 #ifdef FAST_DI_ENABLE_GLOBAL_STATIC_DI
-#include "fast_di/static/DIContainer.hpp"
 #else
-#include "include/fast_di/dynamic/implementation/DIContainer.hpp"
+
+#include "fast_di/dynamic/FastDI.hpp"
+#include <memory>
+
+namespace fast_di::global_di
+{
+
+class GlobalDI final
+{
+private:
+    GlobalDI() = default;
+
+public:
+    GlobalDI(const GlobalDI&) = delete;
+    GlobalDI(GlobalDI&&) = delete;
+    GlobalDI& operator=(const GlobalDI&) = delete;
+    GlobalDI& operator=(GlobalDI&&) = delete;
+
+public:
+    static dynamic_di::DIContainerBuilder& builder()
+    {
+        return get_instance().builder_;
+    }
+
+    static dynamic_di::DIContainer build_container()
+    {
+        return builder().build();
+    }
+
+    static GlobalDI& get_instance()
+    {
+        static GlobalDI global_di;
+        return global_di;
+    }
+
+private:
+    dynamic_di::DIContainerBuilder builder_;
+};
+
+#define DI_SUBSCRIBER_FOR(Dependency) \
+void subscribe_##Dependency()
+
+}
 #endif
 
 #endif //GLOBALDI_HPP_
